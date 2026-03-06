e20420e36c576e09dbc91673a8cf79300d835b5c
aws-nassar <aws.a.nasssar@gmail.com>
2026-03-06 21:39:08 +0200

    CH2: L1

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./api/middleware.js";
 
 const app = express();
 const PORT = 8080;
 
+app.use(middlewareLogResponses);
+app.get("/healthz", handlerReadiness);
 app.use("/app", express.static("./src/app"));
 
-app.get("/healthz", handlerReadiness);
-
-
 app.listen(PORT, () => {
-   console.log(`Server is running at http://localhost:${PORT}/app/index.html`);
- });
+   console.log(`Server is running...`);
+});
